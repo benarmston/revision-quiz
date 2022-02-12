6db6f33f46560720dc47dfd8a1b4e1fc3637477d
Matti Armston <matti.armston@gmail.com>
2022-02-12 11:33:43 +0000

    Added support for choosing subjects to revise.
    I added support for having subject specific questions and being able to
    choose to revise individual subjects either through command line
    arguments or a text prompt at the start.
    
    I could also add support for revising particular topics in subjects e.g.
    Hitler's foreign policy or Infection and Response rather than a whole
    subject at once.
    
    However, at the moment I think I will continue to try to port this
    program over to go. When that is done, I will likely try to move the
    questions to either a json or a yaml file so they can be easily modified
    even with a compiled executable. I will then look into how best I can
    package this program and how best I can distribute it to windows and
    other OSs. I may then add support for single topic revision.

--- python/__main__.py
+++ python/__main__.py
@@ -3,50 +3,95 @@
 import random
 import argparse
 
-from questions import questions
+from questions import Questions
 
 def parseArgs():
     parser = argparse.ArgumentParser(
         description='Revision quiz to test knowledge on various subjects.'
     )
     parser.add_argument('--version', action='store_true', help='output version information and exit')
+    parser.add_argument('--history', action='store_true', help='include history questions')
+    parser.add_argument('--science', '-s', action='store_true', help='inlude science questions')
     return parser.parse_args()
 
-def loadGame(args):
-    def version():
-        print("Version {}".format('0.4.0'))
-        print("This project is still in active development")
-        print("Visit https://github.com/crus4d3/revision-quiz for the latest version")
+def version():
+    print("Version {}".format('0.4.0'))
+    print("This project is still in active development")
+    print("Visit https://github.com/crus4d3/revision-quiz for the latest version")
 
-    def quiz():
-        response = ''
-        score = 0
-        print("Please enter all answers without any trialling spaces and as concisely as possible.")
-        print("Type 'quit' to quit at any time.")
-        keys = list(questions.keys())
-        random.shuffle(keys)
-        for i in keys:
-            question = i
-            answers = questions[i]
-            response = input(question)
-            if response.lower() in ['q', 'quit']:
-                break
-            elif response.lower() in answers:
-                print('Correct!')
-                score += 1
+def getSubjects(questions):
+    done = False
+    subjectList = []
+    while done == False:
+        print("Which subjects do you want to revise 'history', 'science' or 'all'?")
+        subject = input("Type 'done' to continue\n")
+        if subject in ['h', 'history']:
+            print("History chosen!")
+            subjectList.append(questions.historyQuestions)
+        elif subject in ['s', 'science']:
+            print("Science chosen")
+            subjectList.append(questions.scienceQuestions)
+        elif subject in ['a', 'all']:
+            print("All chosen!")
+            done = True
+        elif subject in ['d', 'done']:
+            if subjectList != []:
+                done = True
+                print("Subjects chosen!")
             else:
-                print('Incorrect')
-                print('The correct answers were', answers)
-        print('You got:', score, 'answers correct.')
+                print("You must chose at least one subject")
+        elif subject in ['q', 'quit']:
+            quit()
+        else:
+            print("Unkown option")
+    return subjectList
 
-    if args.version:
-        version()
-    else:
-        quiz()
+def quiz(questions):
+    response = ''
+    score = 0
+    total = 0
+    print("\nPlease enter all answers without any trialling spaces and as concisely as possible.")
+    print("Type 'quit' to quit at any time.")
+    keys = list(questions.keys())
+    random.shuffle(keys)
+    for i in keys:
+        question = i
+        answers = questions[i]
+        response = input(question)
+        if response.lower() in ['q', 'quit']:
+            break
+        elif response.lower() in answers:
+            print('Correct!')
+            score += 1
+        else:
+            print('Incorrect')
+            print('The correct answers were', answers)
+        total += 1
+    print('You got:', score, 'out of', total, 'questions correct.')
 
 def main():
     args = parseArgs()
-    loadGame(args)
+    questionList = []
+    questionDict = {}
+    questions = Questions()
+    if args.version:
+        version()
+        quit()
+    if args.history:
+        questionList.append(questions.historyQuestions)
+    if args.science:
+        questionList.append(questions.scienceQuestions)
+    elif questionList == []:
+        subjectList = getSubjects(questions)
+        if subjectList == []:
+            for question in questions.questionList:
+                questionList.append(question)
+        else:
+            for question in subjectList:
+                questionList.append(question)
+    for d in questionList:
+        questionDict.update(d)
+    quiz(questionDict)
 
 if __name__ == '__main__':
     main()
--- python/questions.py
+++ python/questions.py
@@ -1,11 +1,19 @@
-questions = {
-    'When was the Treaty of Versailles? \n' : ['1919'],
-    'Who was the Treaty of Versailles with? \n' : ['germany'],
-    'Who was the Treaty of Saint Germain with? \n' : ['austria'],
-    'Who was the Treaty of Neuilly with? \n' : ['bulgaria'],
-    'Who was the Treaty of Sevres with? \n' : ['turkey'],
-    'Who was the Treaty of Lausanne with? \n' : ['turkey'],
-    'Who was the leader of Britain in the Paris peace conference? \n' : ['david lloyd george', 'lloyd george'],
-    'Who was the leader of France in the Paris peace conference? \n' : ['georges clemenceau', 'clemenceau'],
-    'Who was the leader of the USA in the Paris peace conference? \n' : ['woodrow wilson', 'wilson'],
-}
+class Questions:
+    def __init__(self):
+        self.historyQuestions = {
+            "When was the Paris Peace Conference?\n" : ["1919"],
+            "Which major power was not invited to the Paris Peace Conference?\n" : ["germany"],
+            "Who was the Treaty of Versailles with?\n" : ["germany"],
+            "Who was the Treaty of Saint Germain with?\n" : ["austria"],
+            "Who was the Treaty of Neuilly with?\n" : ["bulgaria"],
+            "Who was the Treaty of Sevres with?\n" : ["turkey"],
+            "Who was the Treaty of Lausanne with?\n" : ["turkey"],
+            "Who was the leader of Britain in the Paris peace conference?\n" : ["david lloyd george", "lloyd george"],
+            "Who was the leader of France in the Paris peace conference?\n" : ["georges clemenceau", "clemenceau"],
+            "Who was the leader of the USA in the Paris peace conference?\n" : ["woodrow wilson", "wilson"],
+        }
+        self.scienceQuestions = {
+            "What process removes carbon dioxide from the atmosphere?\n" : ["photosynthesis"],
+            "How is energy lost along a food chain?\n" : ["respiration", "urine", "faeces", "not all biomass eaten"],
+        }
+        self.questionList = [self.historyQuestions, self.scienceQuestions]
