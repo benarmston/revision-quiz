969fd3c11bda0d88b9f3e630affa4bb04a07e691
Matti Armston <matti.armston@gmail.com>
2022-02-12 12:00:15 +0000

    Continued work on port to go
    I continued trying to port the program over to go and have seen much
    more success than in my attempt to port it over to c.

--- go/main.go
+++ go/main.go
@@ -2,15 +2,14 @@ package main
 
 import (
     "fmt"
+    "strings"
     "os"
-    "github.com/crus4d3/revision-quiz/go/quiz"
-    //"github.com/crus4d3/revision/quiz"
-    //"github.com/crus4d3/revision-quiz"
-//    "programming/revision-quiz/questions.go"
+    "time"
+    "math/rand"
+    "github.com/crus4d3/revision-quiz/go/questions"
 )
 
 func main() {
-
     progName := os.Args[0]
     args := os.Args[1:]
 
@@ -20,18 +19,19 @@ func main() {
                 help(progName)
             } else if args[i] == "--version" {
                 version()
+            } else if args[i] == "--test" {
+                test()
             } else {
                 unkownArg(args[i])
             }
         }
     } else {
+        rand.Seed(time.Now().UnixNano())
         runQuiz()
     }
 }
 
 func help (name string) {
-    // fmt.Println("Howdy cowboy!")
-    // fmt.Println("Hope that helped!!")
     fmt.Printf("usage: %s [--help] [--version]\n\n", name)
     fmt.Println("Revision quiz to test knowledge on various subjects\n")
     fmt.Println("optional arguments:")
@@ -45,16 +45,70 @@ func version () {
     fmt.Println("Visit https://github.com/crus4d3/revision-quiz for the latest version")
 }
 
+func test () bool {
+    fmt.Println("Running tests")
+    test1 := test1()
+    if test1 {
+        fmt.Println("Test 1 passed")
+    } else {
+        fmt.Println("Test 1 failed")
+    }
+    fmt.Println("Tests finished")
+    return test1
+}
+
+func test1 () bool {
+    slice1 := []string{"a"}
+    contains(slice1)
+    return true
+}
+
 func unkownArg (arg string) {
     fmt.Printf("Unkown arg '%s'\n", arg)
 }
 
 func runQuiz () {
-    fmt.Println(quiz.Questions)
-    response := ""
+    var response string
     score := 0
+    quitMSG := []string{"q", "quit"}
     fmt.Println("Please enter all answers without any trialling spaces and as concisely as possible.")
     fmt.Println("Type 'quit' to quit at any time.")
-    fmt.Printf("Response %s score %d\n", response, score)
+    keys := getRandKeys(questions.Questions)
+    for _, key := range keys {
+        question := key
+        answers := questions.Questions[key]
+        fmt.Println(question)
+        fmt.Scanln(&response)
+        if contains(strings.ToLower(response), quitMSG) {
+            break
+        }
+        if contains(strings.ToLower(response), answers){
+            fmt.Println("Correct!")
+            score += 1
+        } else {
+            fmt.Println("Incorrect")
+            fmt.Printf("The correct answers were %#v\n", answers)
+        }
+    }
+    fmt.Printf("You got: %d answers correct.\n", score)
 }
 
+func contains (search string, slice []string) bool {
+    for _, value := range slice {
+        if value == search {
+            return true
+        }
+    }
+    return false
+}
+
+func getRandKeys (questions map[string][]string) []string {
+    keys := make([]string, len(questions))
+    i := 0
+    for k := range questions {
+        keys[i] = k
+        i++
+    }
+    rand.Shuffle(len(keys), func(i, j int) {keys[i], keys[j] = keys[j], keys[i]})
+    return keys
+}
---
+++ go/questions/questions.go
@@ -0,0 +1,13 @@
+package questions
+
+var Questions = map[string][]string{
+    "When was the Treaty of Versailles?" : {"1919"},
+    "Who was the Treaty of Versailles with?" : {"germany"},
+    "Who was the Treaty of Saint Germain with?" : {"austria"},
+    "Who was the Treaty of Neuilly with?" : {"bulgaria"},
+    "Who was the Treaty of Sevres with?" : {"turkey"},
+    "Who was the Treaty of Lausanne with?" : {"turkey"},
+    "Who was the leader of Britain in the Paris peace conference?" : {"david lloyd george", "lloyd george"},
+    "Who was the leader of France in the Paris peace conference?" : {"georges clemenceau", "clemenceau"},
+    "Who was the leader of the USA in the Paris peace conference?" : {"woodrow wilson", "wilson"},
+}
